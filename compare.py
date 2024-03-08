fd8eaaad7a1f62828b847cabe68f0005f894b6cf
stuart-hahn <47613093+stuart-hahn@users.noreply.github.com>
2024-03-08 10:52:59 -0500

    add compare file that checks if x is equal to y

---
+++ compare.py
@@ -0,0 +1,14 @@
+x = int(input("What's x? "))
+y = int(input("What's y? "))
+
+# if x > y:
+#     print(f"{x} is greater than {y}.")
+# elif y > x:
+#     print(f"{x} is less than {y}.")
+# else:
+#     print(f"{x} is equal to {y}")
+
+if x != y:
+    print(f"{x} is not equal to {y}")
+else:
+    print(f"{x} is equal to {y}.")
