97a553c63074296455c31b317174b91e634cfb22
stuart-hahn <47613093+stuart-hahn@users.noreply.github.com>
2024-03-07 20:42:34 -0500

    refactored and added a string slicing solve

--- bank.py
+++ bank.py
@@ -1,20 +1,45 @@
 def get_greeting():
-    greeting = input("Greeting: ")
-    return greeting.strip().lower()
+    user_input = input("Greeting: ")
+    return user_input.strip().lower()
 
 
 def calculate_balance():
-    greeting = get_greeting()
-    if greeting.startswith("hello"):
+    user_input = get_greeting()
+    hello_length = len("hello")
+
+    if user_input[:hello_length] == "hello":
         print("$0")
-    elif greeting[0] == "h":
+    elif user_input.startswith("h"):
         print("$20")
     else:
         print("$100")
+
+
+# Solve using .startswith() method
+# def calculate_balance():
+#     greeting = get_greeting()
+#     if greeting.startswith("hello"):
+#         print("$0")
+#     elif greeting[0] == "h":
+#         print("$20")
+#     else:
+#         print("$100")
+
+
+# Solve using string slicing
+# def calculate_balance():
+#     greeting = get_greeting()
+#     if greeting[: len("hello")] == "hello":
+#         print("$0")
+#     elif greeting[0] == "h":
+#         print("$20")
+#     else:
+#         print("$100")
 
 
 def main():
     calculate_balance()
 
 
-main()
+if __name__ == "__main__":
+    main()
