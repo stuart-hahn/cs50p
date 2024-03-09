d4ac77fba76ada24a7a870da6addf87a132b5099
stuart-hahn <47613093+stuart-hahn@users.noreply.github.com>
2024-03-09 14:44:46 -0500

    update with more advanced examples

--- loops.py
+++ loops.py
@@ -1,14 +1,26 @@
 def main():
-    # 'for' loop to iterate through a list
-    numbers = [1, 2, 3, 4, 5]
-    for num in numbers:
-        print(num)
+    # # 'for' loop to iterate through a list
+    # numbers = [1, 2, 3, 4, 5]
+    # for num in numbers:
+    #     print(num)
 
-    # 'while' loop to count to 5
-    i = 0
-    while i < 5:
-        print(i + 1)
-        i += 1
+    # # 'while' loop to count to 5
+    # i = 0
+    # while i < 5:
+    #     print(i + 1)
+    #     i += 1
+    # for i in range(5):
+    #     print(i)
+    # for i in range(10, 0, -1):
+    #     print(i)
+    # for i in range(0, 10, 2):  # Increment by 2
+    #     print(i)
+    # my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    # for i in range(0, len(my_list), 2):  # Access every other element
+    #     print(my_list[i])
+    fruits = ["apple", "banana", "cherry"]
+    for index, fruit in enumerate(fruits):
+        print(index, fruit)
 
 
 if __name__ == "__main__":
