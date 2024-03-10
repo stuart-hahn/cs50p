f478712d9b8d5f4c2a12c80926738cbfa3c64a34
stuart-hahn <47613093+stuart-hahn@users.noreply.github.com>
2024-03-10 17:59:45 -0400

    add mario example for printing n number of blocks

---
+++ mario.py
@@ -0,0 +1,22 @@
+def main():
+    print_column(3)
+    print_row(4)
+    print_square(3)
+
+
+def print_column(height):
+    for _ in range(height):
+        print("#")
+
+
+def print_row(width):
+    print("?" * width)
+
+
+def print_square(size):
+    for _ in range(size):
+        print("#" * size)
+
+
+if __name__ == "__main__":
+    main()
