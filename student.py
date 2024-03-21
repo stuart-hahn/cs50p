8eca35b6238132e59dccf87d86848779211741ba
stuart-hahn <47613093+stuart-hahn@users.noreply.github.com>
2024-03-21 18:23:33 -0400

    basic implementation of Student class

--- student.py
+++ student.py
@@ -1,12 +1,16 @@
+class Student: ...
+
+
 def main():
     student = get_student()
-    print(f"{student['name']} from {student['house']}")
+    print(f"{student.name} from {student.house}")
 
 
 def get_student():
-    name = input("Name: ")
-    house = input("House: ")
-    return {"name": name, "house": house}
+    student = Student()
+    student.name = input("Name: ")
+    student.house = input("House: ")
+    return student
 
 
 if __name__ == "__main__":
