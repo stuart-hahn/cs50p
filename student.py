d52c5938d9ccbbd818229c6ba241f9c691fa947a
stuart-hahn <47613093+stuart-hahn@users.noreply.github.com>
2024-03-22 13:17:03 -0400

    create getter, setter, and delete attributes for Student class

--- student.py
+++ student.py
@@ -1,16 +1,48 @@
-class Student: ...
+class Student:
+    def __init__(self, name, house):
+        self.name = name
+        self.house = house
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, name):
+        if not name:
+            raise ValueError("Missing Name")
+        self._name = name
+
+    # Getter
+    @property
+    def house(self):
+        return self._house
+
+    # Setter
+    @house.setter
+    def house(self, house):
+        if house not in ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]:
+            raise ValueError("Invalid house")
+        self._house = house
+
+    # Deleter
+    @house.deleter
+    def house(self):
+        del self._house
+
+    def __str__(self):
+        return f"{self.name} from {self.house}"
 
 
 def main():
     student = get_student()
-    print(f"{student.name} from {student.house}")
+    print(student)
 
 
 def get_student():
-    student = Student()
-    student.name = input("Name: ")
-    student.house = input("House: ")
-    return student
+    name = input("Name: ")
+    house = input("House: ")
+    return Student(name, house)
 
 
 if __name__ == "__main__":
